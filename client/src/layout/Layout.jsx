242835d3835ec481c5469263c5d83c488a7819ee
EliorAvisrur <elior261200@gmail.com>
2024-12-23 09:09:04 +0200

    adding layout

--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "../styles/Layout.module.css";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import { Outlet, NavLink } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 const Layout = () => {
   return (
     <div>
