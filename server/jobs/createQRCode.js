2bd335e7c8e83c6f7633d854f74962c34d589657
yaakov15444 <yakov15444@gmail.com>
2025-01-01 18:08:17 +0200

    ver

--- server/jobs/createQRCode.js
+++ server/jobs/createQRCode.js
@@ -1,9 +1,9 @@
 const QRCode = require("qrcode");
-const {base_url_client} = require("../secrets/dotenv");
+const { base_url_client } = require("../secrets/dotenv");
 // פונקציה ליצירת QR Code
 async function createQRCode() {
     try {
-        const url = `${base_url_client}/gymVisit`; 
+        const url = `${base_url_client}/phoneLogin`;
 
         // יצירת ה-QR Code
         const qrCode = await QRCode.toDataURL(url);
